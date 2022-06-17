34ec6303b39b24cee8516cb66bd8be852634a720
Kacper Wochniak <78349074+PandaDex@users.noreply.github.com>
2022-06-17 12:46:05 +0200

    v1.5

--- src/main/java/me/dex/exs/essentials/commands/exs.java
+++ src/main/java/me/dex/exs/essentials/commands/exs.java
@@ -1,5 +1,6 @@
 package me.dex.exs.essentials.commands;
 
+import me.dex.exs.essentials.Utils.Updater;
 import me.dex.exs.essentials.main.Main;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -17,13 +18,54 @@ public class exs implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player p = (Player) sender;
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Autor: &aPandaDex_"));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Wersja: &a" + this.plugin.getDescription().getVersion()));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Strona: &a" + this.plugin.getDescription().getWebsite()));
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
-        return true;
-    }
+            if(args.length == 0){
+                Player p = (Player) sender;
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Autor: &aPandaDex_"));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Wersja: &a" + this.plugin.getDescription().getVersion()));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Strona: &a" + this.plugin.getDescription().getWebsite()));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
+                return true;
+            }
 
+            if(args[0].equalsIgnoreCase("info")) {
+                Player p = (Player) sender;
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Autor: &aPandaDex_"));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Wersja: &a" + this.plugin.getDescription().getVersion()));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Strona: &a" + this.plugin.getDescription().getWebsite()));
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&c-----------------------------------------------------"));
+                return true;
+            }
+
+            if(args[0].equalsIgnoreCase("update")) {
+            Player p = (Player) sender;
+            if(!p.hasPermission("exs.update")) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] &cNie masz uprawnien do tej komendy"));
+                return true; }
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Wyszukiwanie aktualizacji..."));
+            (new Updater(this.plugin, 101530)).getLatestVersion(version -> {
+                if(!this.plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Twoja wersja EXS jest &cnieaktualna"));
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Nowa wersja: &a" + version));
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Twoja wersja: &c" + this.plugin.getDescription().getVersion()));
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Zaktualizuj tutaj:&a https://www.spigotmc.org/resources/exs-essentials.101530/"));
+                }
+            });
+            return true;
+            }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+            Player p = (Player) sender;
+            if(!p.hasPermission("exs.reload")) {
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] &cNie masz uprawnien do tej komendy"));
+                return true; }
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] Przeladowanie..."));
+            this.plugin.reloadConfig();
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c&lEXS&7] &aPrzeladowano!"));
+            this.plugin.PRNTCON(ChatColor.GREEN + "Przeladowano!");
+            return true;
+            }
+            return false;
+    }
 }
